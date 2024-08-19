404efdf5dbf5ccaa38c53746454bc4c745f9302b
FedorProg <FedorGermanGut@gmail.com>
2024-08-19 16:52:14 +0300

    Create LessonNumber1.py
    Первый урок.

---
+++ LessonNumber1.py
@@ -0,0 +1,17 @@
+# https://metanit.com/python/tutorial/2.2.php
+# Первый урок / Переменные и типы данных
+
+username = "Fedor" # String / Строка
+LikeDogs = True # Bool(Boolean) / Логический тип данных
+number = 13 # Int(Integer) / Целые числа
+number_float = 13.5 # Float / Дробные числа
+complexNumber = 1+2j # Complex / Комплексные числа
+
+path = r"C:\python\name.txt" # Если нужно вставлять путь то передним r
+
+# Чтобы вставить в перменную занчение других перменных, нужно написать user = f"name: {userName}  age: {userAge}"
+# С помощью встроенной функции type() динамически можно узнать текущий тип переменной:
+
+
+
+print("Username: " + username)
